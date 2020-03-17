8429885100037442b0525cc89786a5113bab131c
Robert Seymour <robbyseymour@gmail.com>
2020-03-17 14:09:29 +0000

    Fix for MATLAB 2016b
    #14
    - Changed " to '
    - Added tsvread file for earlier versions of MATLAB

--- data_functions/ft_read_json.m
+++ data_functions/ft_read_json.m
@@ -1,13 +1,14 @@
 function meg    = ft_read_json(filename)
 %% Setup the Import Options
-opts                    = delimitedTextImportOptions("NumVariables", 1);
+try
+opts                    = delimitedTextImportOptions('NumVariables', 1);
 opts.DataLines          = [1, Inf];
-opts.Delimiter          = "";
-opts.VariableTypes      = "string";
-opts                    = setvaropts(opts, 1, "WhitespaceRule", "preserve");
-opts                    = setvaropts(opts, 1, "EmptyFieldRule", "auto");
-opts.ExtraColumnsRule   = "ignore";
-opts.EmptyLineRule      = "read";
+opts.Delimiter          = '';
+opts.VariableTypes      = 'string';
+opts                    = setvaropts(opts, 1, 'WhitespaceRule', 'preserve');
+opts                    = setvaropts(opts, 1, 'EmptyFieldRule', 'auto');
+opts.ExtraColumnsRule   = 'ignore';
+opts.EmptyLineRule      = 'read';
 
 % Import the data
 meg         = readtable(filename, opts);
@@ -15,3 +16,6 @@ meg         = readtable(filename, opts);
 %% Convert to output type
 meg         = table2array(meg);
 meg         = jsondecode(meg);
+catch
+    meg = jsondecode(fileread(filename));
+end
