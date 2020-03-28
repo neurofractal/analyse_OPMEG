cee2264c647fbe9541ae56cfeca192ceef684d70
Robert Seymour <robbyseymour@gmail.com>
2020-03-28 18:33:46 +0000

    Add extra option cfg.return_all
    - This allows the user to specify whether they want to return all channels or just those specified in cfg.channel.
    
    - Also tidied up code formatting a little!

--- data_functions/ft_opm_synth_gradiometer.m
+++ data_functions/ft_opm_synth_gradiometer.m
@@ -14,7 +14,10 @@ function [data_out] = ft_opm_synth_gradiometer(cfg,data_in)
 %                       indicate frequency in Hz (0-30Hz, 40-60Hz,
 %                       110-130Hz). (Default = []).
 %   cfg.derivative      = add the derivative of the reference signal to the
-%                       regressors (default = 'no');
+%                       regressors (default = 'no').
+%   cfg.return_all      = 'yes' or 'no' - do you want to return all
+%                       channels, not just those specified with 
+%                       cfg.channel? (default = 'no').
 %__________________________________________________________________________
 % Copyright (C) 2020 Wellcome Trust Centre for Neuroimaging
 % Adapted from spm_opm_synth_gradiometer (Tim Tierney)
@@ -23,7 +26,6 @@ function [data_out] = ft_opm_synth_gradiometer(cfg,data_in)
 %           Tim West            (timothy.west@ndcn.ox.ac.uk)
 %__________________________________________________________________________
 
-data_out = data_in; % Make temp copy (retains non MEG Channels)
 %% Set default values
 if ~isfield(cfg, 'channel')
     cfg.channel = 'MEG';
@@ -41,9 +43,13 @@ if ~isfield(cfg, 'derivative')
     cfg.derivative = 'no';
 end
 
-filter_ref  = cfg.filter_ref;
-channel     = cfg.channel;
-refchannel  = cfg.refchannel;
+if ~isfield(cfg, 'return_all')
+    cfg.return_all = 'no';
+end
+
+filter_ref          = cfg.filter_ref;
+channel             = cfg.channel;
+refchannel          = cfg.refchannel;
 
 %% Select MEG data and reference data
 % MEG data
@@ -51,35 +57,43 @@ if strcmp(channel,'all')
     
     ft_warning('Selecting MEG channels');
     
-    cfg2 = [];
-    cfg2.channel = ft_channelselection_opm('MEG',data_in);
-    meg_data = ft_selectdata(cfg2,data_in);
+    cfg2            = [];
+    cfg2.channel    = ft_channelselection_opm('MEG',data_in);
+    meg_data        = ft_selectdata(cfg2,data_in);
     
 else
-    cfg2 = [];
-    cfg2.channel = ft_channelselection_opm(channel,data_in);
-    meg_data = ft_selectdata(cfg2,data_in);
+    cfg2            = [];
+    cfg2.channel    = ft_channelselection_opm(channel,data_in);
+    meg_data        = ft_selectdata(cfg2,data_in);
 end
 
 fprintf('Selected %2d data channels\n',length(meg_data.label));
 
 % Reference data
-cfg2 = [];
-cfg2.channel = ft_channelselection_opm(refchannel,data_in);
-ref_data = ft_selectdata(cfg2,data_in);
+cfg2                = [];
+cfg2.channel        = ft_channelselection_opm(refchannel,data_in);
+ref_data            = ft_selectdata(cfg2,data_in);
 fprintf('Selected %2d reference channels\n',length(ref_data.label));
 
-%%
+% Save data for later
+if strcmp(cfg.return_all,'yes')
+    data_out        = data_in;
+    ch_indx         = contains(data_in.label,meg_data.label);
+else
+    data_out        = meg_data;
+end
+
+%% Start of the trial loop
 for tr = 1:numel(ref_data.trial)
-    megind = meg_data.trial{tr};
-    megres = zeros(size(megind));
-    ref = ref_data.trial{tr};
+    megind          = meg_data.trial{tr};
+    megres          = zeros(size(megind));
+    ref             = ref_data.trial{tr};
     
-    ref_size = size(ref,1);
-    winSize = size(megind,2);
+    ref_size        = size(ref,1);
+    winSize         = size(megind,2);
     
     % add a mean column to the reference regressors
-    intercept = ones(winSize,1);
+    intercept       = ones(winSize,1);
     
     %%
     % If the user wants to filter the reference data...
@@ -93,9 +107,10 @@ for tr = 1:numel(ref_data.trial)
         
         % For every pair of frequencies...
         for filt = 1:size(filter_ref,1)
-            
-            fprintf('Filtering reference data: %3dHz - %3dHz ... \n',...
-                filter_ref(filt,1),filter_ref(filt,2));
+            if tr == 1
+                fprintf('Filtering reference data: %3dHz - %3dHz ... \n',...
+                    filter_ref(filt,1),filter_ref(filt,2));
+            end
             
             data_filt = ft_preproc_highpassfilter(ref,data_in.fsample,...
                 filter_ref(filt,1),5,'but','twopass','reduce');
@@ -108,7 +123,6 @@ for tr = 1:numel(ref_data.trial)
         
         reference = reference';
         
-        
     else
         
         reference = ref';
@@ -116,22 +130,27 @@ for tr = 1:numel(ref_data.trial)
     
     %%
     if strcmp(cfg.derivative,'yes')
-        drefer = diff(reference);
-        drefer = [drefer(1,:); drefer];
-        reference = [drefer reference];
+        drefer      = diff(reference);
+        drefer      = [drefer(1,:); drefer];
+        reference   = [drefer reference];
     end
     
     %%
     % Here we are actually doing the regression
-    disp('Regressin''...');
-    reference= [reference ones(size(reference,1),1)];
+    if tr == 1
+        disp('Regressin''...');
+    end
+    reference       = [reference ones(size(reference,1),1)];
     % reference is column major so transpose sensors
-    beta = pinv(reference)*megind';
-    regressed_data = (megind'- reference*beta)';
-    
-    
+    beta            = pinv(reference)*megind';
+    regressed_data  = (megind'- reference*beta)';
+        
     %% Return the data!
-    data_out.trial{tr}(cfg.chinds,:) = regressed_data;
+    if strcmp(cfg.return_all,'yes')
+        data_out.trial{tr}(ch_indx,:) = regressed_data;
+    else
+        data_out.trial{tr} = regressed_data;
+    end
 end
 
 
