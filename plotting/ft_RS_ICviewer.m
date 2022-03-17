cdbabea60fce76b5061589732e42bb2b718e359d
Robert Seymour <robbyseymour@gmail.com>
2022-03-17 10:47:35 +0000

    ICviewer now with subfunctions

--- plotting/ft_RS_ICviewer.m
+++ plotting/ft_RS_ICviewer.m
@@ -1,13 +1,21 @@
-function [comp2remove] = ft_RS_ICviewer(cfg,comp,data_ICA)
-% Function to display ICs and select which to remove
+function [comp2remove] = ft_RS_ICviewer(cfg,comp)
+% Function to display ICs with a GUI, with options for KEEP/REMOVE.
+% Output is an array of the comps to be rejected.
+%
+% Currently tested on MATLAB 2021b, Fieldtrip v20210606. Will work best for
+% continuous datasets, rather than datasets already broken up into 
+% shorter epochs.
 %
 % EXAMPLE USEAGE:   [comp2remove] = ft_RS_ICviewer(cfg,comp)
 % ...where, cfg is the input structure and comp is the output from
 % ft_componentanalysis
-%   cfg.layout          = lay;
-%   cfg.foi             = frequencies of interest in form [X Y].
+%   cfg.layout          = layout file created by ft_prepare_layout
+%   cfg.foi             = frequencies of interest in form [X Y] for PSD
 %                       (default = [1 100]).
+%   cfg.trial_length    = size of chunks used for PSD calculation
+%                       (default = 10)
 %   cfg.save            = save a .png of removed comps? (default = 'no')
+%   
 %__________________________________________________________________________
 % Copyright (C) 2022 Wellcome Trust Centre for Neuroimaging
 
@@ -17,6 +25,10 @@ function [comp2remove] = ft_RS_ICviewer(cfg,comp,data_ICA)
 %% Function housekeeping
 if ~isfield(cfg, 'foi')
     cfg.foi = [1 100];
+end
+
+if ~isfield(cfg, 'trial_length')
+    cfg.trial_length = 10;
 end
 
 if ~isfield(cfg, 'save')
@@ -29,13 +41,15 @@ end
 
 %% Start of function proper
 % Calculate PSD
+
 cfg2                 = [];
 cfg2.channel         = 'all';
-cfg2.trial_length    = 10;
-cfg2.method          = 'tim';
+cfg2.trial_length    = cfg.trial_length;
 cfg2.foi             = [cfg.foi(1) cfg.foi(2)];
 cfg2.plot            = 'no';
-[pow freq]          = ft_opm_psd(cfg2,data_ICA);
+[pow, freq]          = calc_PSD(cfg2,comp);
+
+% Make sure there are no nans (and take mean)
 po                  = nanmean(pow(:,:,:),3);
 
 % Use Brewermap :colors RdBu
@@ -51,7 +65,7 @@ S.f = figure;
 
 while cont
 
-    set(gcf, 'Position',  [300, 0, 1100, 900]);
+    set(gcf, 'Position',  [300, 100, 1100, 900]);
     %create two pushbttons
     S.pb = uicontrol(S.f,'style','togglebutton',...
         'units','pix',...
@@ -60,7 +74,7 @@ while cont
         'fontsize',14,...
         'Tag','REMOVE',...
         'string','REMOVE',...
-        'callback',@pb_call2);
+        'callback',@pb_remove);
 
     S.pb = uicontrol(S.f,'style','push',...
         'units','pix',...
@@ -69,16 +83,16 @@ while cont
         'fontsize',14,...
         'Tag','flip_button',...
         'string','KEEP',...
-        'callback',@pb_call);
+        'callback',@pb_keep);
 
         S.pb = uicontrol(S.f,'style','push',...
         'units','pix',...
-        'position',[0 300 150 40],...
+        'position',[20 300 100 40],...
         'backgroundcol',[1 1 1],...
         'fontsize',14,...
         'Tag','EXIT',...
         'string','EXIT',...
-        'callback',@pb_call3);
+        'callback',@pb_exit);
 
     % Find limits of y-axis
     [minDistance, indexOfMin] = min(abs(freq-cfg.foi(1)));
@@ -110,22 +124,21 @@ while cont
     xlabel('Time(s)','FontSize',23);
     ylabel({'Magnetic';'Field'},'FontSize',23);
 
-        % Plot PSD
+    % Plot PSD
     subplot(8,4,[1:2 5:6 9:10 13:14]);
     semilogy(freq,po(:,c),'-k','LineWidth',2);
-    xlim([1 80]);
+    xlim([cfg.foi(1) cfg.foi(2)]);
     set(gca,'FontSize',16) % Creates an axes and sets its FontSize to 18
     xlabel('Frequency (Hz)','FontSize',23)
-    labY = ['$$PSD (a.u.) $$)'];
+    labY = ['$$PSD (a.u.) $$'];
     ylabel(labY,'interpreter','latex','FontSize',23)
     ylim([min_lim max_lim]);
-    %print(['component_PSD' num2str(c)],'-dpng','-r300');
-
 
 
     % Wait for user input
     uiwait(S.f)
     
+    % Perform various actions based on the button pressed
     if strcmp(S.f.UserData,'EXIT')
         cont = 0;
     elseif strcmp(S.f.UserData,'REMOVE')
@@ -134,10 +147,10 @@ while cont
         disp(['Component ' num2str(c) ' = BAD']);
         disp('');
 
-         % Save a picture
-    if strcmp(cfg.save, 'yes')
-        print(['component_' num2str(c)],'-dpng','-r300');
-    end
+        % Save a picture if required
+        if strcmp(cfg.save, 'yes')
+            print(['component_' num2str(c)],'-dpng','-r100');
+        end
 
     end
 
@@ -156,5 +169,98 @@ while cont
         close(S.f);
     end
    
+
+end
+
+%% Sub-functions
+function [pow, freq] = calc_PSD(cfg,rawData)
+ % if cfg.trial_length is an empty array we'll get it to guess the
+        % length of the trial
+        if isempty(cfg.trial_length)
+            cfg.trial_length = range(rawData.time{1});
+        end
+        
+        if cfg.trial_length > range(rawData.time{1})
+            error('You are specifying PSD windows longer than the trial length!')
+        end
+        
+        % Split data into epochs
+        nsamps = (cfg.trial_length)*rawData.fsample;
+        ntrials = numel(rawData.trial);
+        
+        for ii = 1:ntrials
+            beg = 1:nsamps:size(rawData.trial{ii},2);
+            endsamp =  beg+(nsamps-1);
+            inRange = ~(beg>=size(rawData.trial{ii},2)|endsamp>=size(rawData.trial{ii},2));
+            chans = 1:1:(size(rawData.trial{ii},1));
+            if ii == 1
+                eD = zeros(length(chans),nsamps,sum(inRange),ntrials);
+            end
+            for jj = 1:length(inRange)
+                if(inRange(jj))
+                    eD(:,:,jj,ii)=rawData.trial{ii}(chans,beg(jj):endsamp(jj),:);
+                end
+            end
+        end
+        
+        % Reshape eD so its 3D
+        eD = reshape(eD,size(eD,1),size(eD,2),[]);
+        
+        % Get variables for calculating PSD
+        fs = rawData.fsample;
+        N = size(eD,2);
+        Nf = ceil((N+1)/2);
+        nepochs = size(eD,3);
+        pow = zeros(Nf,size(eD,1),nepochs);
+        wind  = window(@hanning,size(eD,2));
+        coFac = max(wind)/mean(wind);
+        wind = repmat(wind,1,size(eD,1));
+        
+        % Calculate PSD for each epoch
+        for j = 1:nepochs
+            Btemp=eD(:,:,j)';
+            Btemp = Btemp.*wind*coFac;
+            Btemp = Btemp;
+            
+            mu=median(Btemp);
+            zf = bsxfun(@minus,Btemp,mu);
+            fzf = zf;
+            N= length(fzf);
+            xdft = fft(fzf);
+            xdft=xdft(1:floor(N/2+1),:);
+            psdx = abs(xdft)./sqrt(N*fs);
+            freq = 0:fs/size(fzf,1):fs/2;
+            odd=mod(size(fzf,1),2)==1;
+            if(odd)
+                %psdx(2:end) = sqrt(2)*psdx(2:end);
+                psdx(2:end) = psdx(2:end);
+            else
+                %psdx(2:end-1) = sqrt(2)*psdx(2:end-1);
+                psdx(2:end-1) = psdx(2:end-1);
+            end
+            pow(:,:,j) =psdx;
+        end
+end
+
+function pb_keep(hObject,~)
+    hObject.Parent.UserData = 'moveon';
+    uiresume;
+    return
+    end
+
+function pb_remove(hObject,~)
+    disp('');
+    %At the end of the callback function:
+    hObject.Parent.UserData = 'REMOVE';
+    uiresume;
+    return
+end
+
+function pb_exit(hObject,~)
+    disp('EXIT');
+    hObject.Parent.UserData = 'EXIT';
+    uiresume;
+    return
+end
 
 end
