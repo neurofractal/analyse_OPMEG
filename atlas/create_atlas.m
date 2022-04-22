a47e99e06f39c4443cbc0357e7821a53921e0673
Robert Seymour <robbyseymour@gmail.com>
2022-04-22 10:18:09 +0100

    DK atlas + other atlas stuff

---
+++ atlas/create_atlas.m
@@ -0,0 +1,129 @@
+% 
+cd('D:\Github\analyse_OPMEG\atlas\dk_atlas')
+
+% Add Freesurfer_tools and SPM to your path
+addpath('D:\Github\OPMsurfer\freesurfer_tools');
+addpath('D:\scripts\spm12')
+
+maxDist=5.5; % maximum distance (mm) for projection ? caution, if too large, subcortical structures will also be (falsely) assigned.
+visualize=1; % set to 0 if you don't need to visualize
+reslice=0; % set to 1 if you want to produce an 0.5 mm version, too.
+
+% load in LH
+[~, lhlab,lhctable]=read_annotation('lh.aparc.annot');
+[lhvtx,lhfaces]=read_surf('lh.pial');
+
+% Delete first superfluous row
+lhctable.table(1,:) = [];
+lhctable.struct_names(1,:) = [];
+
+[~,lhix]=ismember(lhlab,lhctable.table(:,5));
+
+% load in RH
+[~, rhlab,rhctable]=read_annotation('rh.aparc.annot');
+[rhvtx,rhfaces]=read_surf('rh.pial');
+
+% Delete first superfluous row
+rhctable.table(1,:) = [];
+rhctable.struct_names(1,:) = [];
+[~,rhix]=ismember(rhlab,rhctable.table(:,5));
+
+% Compensate for number of parcels in LH
+rhix = rhix+max(lhix);
+
+% 
+% % visualize Surfaces
+% figure('Name','Data Conversion','NumberTitle','off','color','w');
+% subplot(1,2,1)
+% title('Surface data');
+% patch('Faces',lhfaces+1,'Vertices',lhvtx,'FaceColor','interp','EdgeColor','none','Facevertexcdata',lhix)
+% patch('Faces',rhfaces+1,'Vertices',rhvtx,'FaceColor','interp','EdgeColor','none','Facevertexcdata',rhix)
+% axis equal
+% axis([-80,80,-110,80])
+
+% load SPM Canonical Brain
+nii=spm_vol('D:\scripts\fieldtrip-20210606\template\anatomy\single_subj_T1.nii');
+suffx='';
+nii.img=spm_read_vols(nii);
+
+% find positive GM voxels (>0.5 probability):
+[xx,yy,zz]=ind2sub(size(nii.img),find(nii.img>0.5));
+XYZ_vx=[xx,yy,zz,ones(length(xx),1)]';
+XYZ_mm=nii.mat*XYZ_vx;
+
+[assign,D]=knnsearch([lhvtx;rhvtx],XYZ_mm(1:3,:)');
+
+
+% remove voxels too far away from vertices (defined by maxDist);
+xx(D>maxDist)=[];
+yy(D>maxDist)=[];
+zz(D>maxDist)=[];
+assign(D>maxDist)=[];
+
+ldat=[lhix;rhix];
+
+% set all voxels to zero
+nii.img(:)=0;
+% assign voxel intensities based on label table
+nii.img(sub2ind(size(nii.img),xx,yy,zz))=ldat(assign);
+
+%nii.img(nii.img>0)=nii.img(nii.img>0)-1; % first entry in table.table is an indexing entry
+
+nii.fname=['aparc_SPM',suffx,'.nii'];
+%nii.dt=[16,0];
+spm_write_vol(nii,nii.img);
+gzip(['aparc_SPM',suffx,'.nii']);
+
+% % visualize volumetric data
+% subplot(1,2,2)
+% title('Volumetric data');
+% for reg=1:max(rhix)
+%     fv=isosurface(nii.img==reg,0.5);
+%     try % swap xy entries for non-empty labels (as per xy swap in isosurface).
+%         fv.vertices=[fv.vertices(:,2),fv.vertices(:,1),fv.vertices(:,3)];
+%     end
+%     afv(reg).vertices=fv.vertices;
+%     afv(reg).faces=fv.faces;
+%     afv(reg).facevertexcdata=repmat(reg,length(fv.vertices),1);
+% end
+% afv=ea_concatfv(afv);
+% 
+% % convert vertices from voxel to mm space
+% afv.vertices=[afv.vertices,ones(length(afv.vertices),1)]';
+% afv.vertices=nii.mat*afv.vertices;
+% afv.vertices=afv.vertices(1:3,:)';
+% patch(afv,'EdgeColor','none','FaceColor','interp')
+% axis equal
+% axis([-80,80,-110,80]); 
+% alpha 0.5;
+
+%% Reead the atlas
+atlas = ft_read_atlas('aparc_SPM.nii');
+count = 1;
+atlas.coordsys = 'mni';
+
+%% Add tissue labels
+% LH
+for h = 1:length(lhctable.struct_names)
+    atlas.tissuelabel{count,1} = ['LH_' lhctable.struct_names{h}];
+    atlas.parcellationlabel{count,1} = ['LH_' lhctable.struct_names{h}];
+    count = count+1;
+end
+
+% RH
+for h = 1:length(rhctable.struct_names)
+    atlas.tissuelabel{count,1} = ['RH_' rhctable.struct_names{h}];
+    atlas.parcellationlabel{count,1} =  ['RH_' rhctable.struct_names{h}];
+    count = count+1;
+end
+
+%% Save
+save atlas_dk_SPM atlas;
+
+%% Try loading in the atlas again
+atlas = ft_read_atlas('atlas_dk_SPM.mat');
+
+
+
+
+
--- source_analysis/atlas2VE.m
+++ source_analysis/atlas2VE.m
@@ -8,11 +8,11 @@ function [VE] = atlas2VE(atlas,template_grid,labels, data_clean,...
 %%%%%%%%%%%
 % Inputs:
 %%%%%%%%%%%
-% - atlas           = atlas you wish to use for VE computation. Should have 
+% - atlas           = atlas you wish to use for VE computation. Should have
 %                   been read into MATLAB using ft_read_atlas
 % - template_grid   = template_grid used for source analysis (read into
 %                   MATLAB from .../fieldtrip-XXXXX/template/sourcemodel/
-%                   standard_sourcemodel3dXmm.mat 
+%                   standard_sourcemodel3dXmm.mat
 % - labels          = name of the ROIs in your atlas
 % - data_clean      = your clean sensor-level data
 % - source_all      = result of ft_sourceanalysis with the filters you wish
@@ -22,12 +22,17 @@ function [VE] = atlas2VE(atlas,template_grid,labels, data_clean,...
 %%%%%%%%%%%
 % Outputs:
 %%%%%%%%%%%
-% - VE              = virtual electrode with label, time, trial and 
+% - VE              = virtual electrode with label, time, trial and
 %                   trialinfo fields
 %
 % Example: [VE] = mq_atlas2VE(atlas,template_grid,labels,data_clean,...
 %                 sourceall,avg_cov);
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% If 'all' specified use all the tissuelabels
+if strcmp(labels,'all')
+    labels = atlas.tissuelabel;
+end
 
 cfg = [];
 cfg.interpmethod = 'nearest';
@@ -41,42 +46,53 @@ hold on;
 
 % Create VE
 VE = [];
-VE.label = labels;
 try
     VE.trialinfo = data_clean.trialinfo;
 catch
 end
 
-for lab = 1:length(labels)
-    
-    fprintf('Computing VE for %10s\n',labels{lab});
-    
-    % Find atlas points
-    atlas_points = find(sourcemodel2.tissue==...
-        find(contains(sourcemodel2.tissuelabel,labels{lab})));
-    
-    ft_plot_mesh(template_grid.pos(atlas_points,:),'vertexsize',10,...
-        'vertexcolor','r'); drawnow; hold on;
+count = 1;
 
-    % Concatenate spatial filter
-    
+for lab = 1:length(labels)
+
+    fprintf('Computing VE for %10s\n',labels{lab});
+
+    % Find atlas points
     try
-    F    = cat(1,sourceall.avg.filter{atlas_points});
+        atlas_points = find(sourcemodel2.tissue==...
+            find(contains(sourcemodel2.tissuelabel,labels{lab})));
+
+        ft_plot_mesh(template_grid.pos(atlas_points,:),'vertexsize',10,...
+            'vertexcolor','r'); drawnow; hold on;
+
+        % Concatenate spatial filter
+
+        try
+            F    = cat(1,sourceall.avg.filter{atlas_points});
+        catch
+            fprintf(['Cannot find the avg.filter ... did you specfify ',...
+                'cfg.keepfilter = yes \nwhen performing ft_sourceanalysis?\n']);
+        end
+
+        % Do SVD
+        [u,s,v] = svd(F*avg_cov.cov*F');
+        filter = u'*F;
+
+        % Create VE
+        for subs=1:(length(data_clean.trial))
+            % note that this is the non-filtered "raw" data
+            VE.time{subs}       = data_clean.time{subs};
+            VE.trial{subs}(count,:) = filter(1,:)*data_clean.trial{subs}(:,:);
+        end
+
+        % Add label
+        VE.label{count,1} = labels{lab};
+        count = count+1;
+
     catch
-        fprintf(['Cannot find the avg.filter ... did you specfify ',...
-            'cfg.keepfilter = yes \nwhen performing ft_sourceanalysis?\n']);
+        warning(['Could not find any atlas points for ROI: ' labels{lab}]);
     end
-    
-    % Do SVD
-    [u,s,v] = svd(F*avg_cov.cov*F');
-    filter = u'*F;
-    
-    % Create VE
-    for subs=1:(length(data_clean.trial))
-        % note that this is the non-filtered "raw" data
-        VE.time{subs}       = data_clean.time{subs};
-        VE.trial{subs}(lab,:) = filter(1,:)*data_clean.trial{subs}(:,:);
-    end
+
 end
 
 
@@ -84,5 +100,5 @@ end
 
 
 
-    
+
 end
