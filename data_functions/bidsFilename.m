6a6a95cb37bf9d657e2150408beb6efeadb1cc78
Nicholas Alexander <nalexander@fil.ion.ucl.ac.uk>
2022-12-08 09:33:36 +0000

    function to create bids filenames
    For saving or loading data during preprocessing or group analysis.

---
+++ data_functions/bidsFilename.m
@@ -0,0 +1,38 @@
+function [filename] = bidsFilename(bids,description,type,derivative)
+% Function to create a BIDS style filename. It is useful for saving and
+% loading data. Use as:
+% Usual bids info as structure containing strings
+% bids.sub			= 'pilot01';
+% bids.ses			= '002';
+% bids.run			= '003';
+% bids.task			= 'oddball';
+% bids.directory	= 'D:\data\MyBidsStudy'; % Folder must exist already
+% description		= 'test'; % Describe the file. e.g. 'freqData'
+% type				= '.mat' % Usually a .mat
+% derivative		= true; % Place in derivative folder or main folder?
+%
+% Author: Nicholas Alexander, n.alexander@ucl.ac.uk
+
+% Check if the initial directory exists. 
+if isfolder(bids.directory)
+	% Check the backslash at the end
+	if bids.directory(end) ~= '\'
+		bids.directory = strcat(bids.directory,'\');
+	end
+	% If using the derivative folder, add that on, including sub
+	if derivative
+		bids.directory = sprintf('%1$sderivatives\\sub-%2$s\\',bids.directory,bids.sub);
+
+		% Check that directory exists with the derivative folder
+		if ~isfolder(bids.directory)
+			warning("Creating new derivatives directory")
+			mkdir(bids.directory);
+		end
+	end
+else
+	error("Specified BIDS directory does not exist");
+end
+
+% Now create the filename
+filename = sprintf('%1$ssub-%2$s_ses-%3$s_task-%4$s_run-%5$s_%6$s%7$s',bids.directory,bids.sub,bids.ses,bids.task,bids.run,description,type);
+
